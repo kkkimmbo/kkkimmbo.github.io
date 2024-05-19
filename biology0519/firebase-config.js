ad34cc737e92e8fba67bfce5b6e42f2574c9cf75
kkkimmbo <168838330+kkkimmbo@users.noreply.github.com>
2024-05-19 14:34:03 +0800

    Add files via upload

---
+++ biology0519/firebase-config.js
@@ -0,0 +1,12 @@
+ㄋ// Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAifZ76m-W79Ptw3gJVGsolZDnoXu72mDc",
+    authDomain: "biologylearning-s11055013.firebaseapp.com",
+    databaseURL: "https://biologylearning-s11055013-default-rtdb.firebaseio.com",
+    projectId: "biologylearning-s11055013",
+    storageBucket: "biologylearning-s11055013.appspot.com",
+    messagingSenderId: "743496923725",
+    appId: "1:743496923725:web:866adef56bd80b02d53a04"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
